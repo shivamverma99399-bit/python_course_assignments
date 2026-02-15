09d55b391d40f28b2fae0c16d530d06496d76c58
shivamverma99399-bit <shivamverma99399@gmail.com>
2026-02-15 20:31:53 +0530

    assignment 1

---
+++ task_1.py
@@ -0,0 +1,22 @@
+'''Problem Statement 1 : Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen.'''
+
+a= float(input("Enter the first number: "))
+b= float(input("Enter the second number: "))
+c= a+b
+d=a-b
+e=a*b
+f=a/b
+print('ADDITION OF TWO NUMBERS IS:',c)
+print('SUBTRACTION OF TWO NUMBERS IS:',d)
+print('MULTIPLICATION OF TWO NUMBERS IS:',e)
+
+print('DIVISION OF TWO NUMBERS IS:',f)
+
+
---
+++ task_2.py
@@ -0,0 +1,9 @@
+first_name = str(input("enter the first name: "))
+last_name = str(input("enter the second name: "))
+print("Hello !! " +first_name+ " "+last_name+ " welcome to the python course")
+
+#using fstirng
+print(f"Hello !! {first_name} {last_name} Welcome to the python program")
+
+#using format() method
+print("Hello !! {} {}. Welcome to the python program".format(first_name , last_name))
