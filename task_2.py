8498e16f3c085445c7767c645dd5b02441aa7896
Yuvraj VIjay Verma <shivamverma99399@gmail.com>
2026-02-15 20:02:34 +0530

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,4 @@
+first_name = str(input("enter the first name: "))
+last_name = str(input("enter the second name: "))
+print("Hello !! " +first_name+ " "+last_name+ " welcome to the python course")
+print(f"Hello !! {first_name} {last_name} Welcome to the python program")
